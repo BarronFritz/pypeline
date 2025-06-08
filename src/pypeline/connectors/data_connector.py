5326849ab6ffed2031e1e5b6d0b4c66a34fd64e6
Barron Fritz <barron.vonn.fritz@gmail.com>
2025-06-08 14:43:28 -0500

    comment and cleanup. Attempt to clean up PypeData's accepted types.

--- src/pypeline/connectors/data_connector.py
+++ src/pypeline/connectors/data_connector.py
@@ -1,16 +1,46 @@
-from ..connector import PypeConnector
-from ..data import PypeData
+"""DataConnector class definition."""
+
 from typing import Any
 
+from pypeline.connector import PypeConnector
+from pypeline.data import PypeData
+
+
 class DataConnector(PypeConnector):
-    def __init__(self, data: PypeData | Any):
+    """Connect to a PypeData source."""
+
+    def __init__(self, data: PypeData | Any) -> None:  # noqa: ANN401
+        """Create a new or reference to existing PypeData.
+
+        Args:
+            data (PypeData | Any): Attempt to convert Any type to PypeData.
+
+        """
         self.data = PypeData(data)
 
     def check(self) -> bool:
-        return self.data.df.shape != (0, 0)
+        """Check if PypeData has data.
+
+        Returns:
+            bool: True if underlying dataframe is not empty.
+
+        """
+        return not self.data.df.is_empty()
 
     def read(self) -> PypeData:
+        """Create new reference to the data.
+
+        Returns:
+            PypeData: new reference to the read data.
+
+        """
         return self.data
 
-    def write(self, data: PypeData):
+    def write(self, data: PypeData) -> None:
+        """Update internal PypeData object reference.
+
+        Args:
+            data (PypeData): New object to reference.
+
+        """
         self.data = data
--- src/pypeline/data.py
+++ src/pypeline/data.py
@@ -1,9 +1,29 @@
+"""PypeData class definition."""
+
+from typing import TYPE_CHECKING
+
 import polars as pl
-from typing import Any
+
+if TYPE_CHECKING:
+    from polars._typing import FrameInitTypes  # type: ignore  # noqa: PGH003
 
 
 class PypeData:
-    def __init__(self, data: Any):
+    """Wrapper for polars DataFrame.
+
+    Which itself is a wrapper around an apache-arrow dataset.
+    """
+
+    def __init__(self, data: "PypeData | FrameInitTypes | pl.DataFrame") -> None:  # type: ignore  # noqa: PGH003
+        """Create a new reference to passed 'data' or construct from source.
+
+        Args:
+            data (Any): PypeData, polars DataFrame, or DataFrame initializer.
+
+        """
         if isinstance(data, PypeData):
-            data = data.df
-        self.df = pl.DataFrame(data)
+            self.df = data.df
+        elif isinstance(data, pl.DataFrame):
+            self.df = data
+        else:
+            self.df = pl.DataFrame(data)
