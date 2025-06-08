484c369e7fe209a865dba3291f24768b19a23bc3
Barron Fritz <barron.vonn.fritz@gmail.com>
2025-06-08 13:23:42 -0500

    moved built in connector connections to test_connectors and performed a comment and cleanup

--- tests/test_connectors.py
+++ tests/test_connectors.py
@@ -1,42 +1,108 @@
-from pypeline.data import PypeData
+"""Test built-in connector classes can init, check, read, and write."""
+
 from pathlib import Path
+from typing import Any
 
-# Test DataConnector
-from pypeline.connectors.data_connector import DataConnector
+from sqlalchemy import inspect
+
 from pypeline.connectors.csv_connector import CSVConnector
+from pypeline.connectors.data_connector import DataConnector
+from pypeline.connectors.sql_connector import SQLConnector
+from pypeline.data import PypeData
 
 
-def test_data_connector():
+def test_data_connector() -> None:
+    """Test operations for the DataConnector.
+
+    Tests init, check, read, and write.
+    """
     # Create test data and setup connector
-    test_data = dict({"col1": [1, 2, 3, 4, 5], "col2": ["a", "b", "c", "d", "e"]})
+    test_data: dict[str, Any] = {
+        "col1": [1, 2, 3, 4, 5],
+        "col2": ["a", "b", "c", "d", "e"],
+    }
     data = PypeData(test_data)
     extractor = DataConnector(data)
 
     # Test data exists
-    assert extractor.check()
+    assert extractor.check()  # noqa: S101
     test_data_2 = test_data
     test_data_2["col1"] = [6, 7, 8, 9, 10]
+
     # Test extractor data is the same as test_data
-    assert extractor.read().df.equals(data.df)
+    assert extractor.read().df.equals(data.df)  # noqa: S101
+
     # Not equal after write
     extractor.write(PypeData(test_data_2))
-    assert not extractor.read().df.equals(data.df)
+    assert not extractor.read().df.equals(data.df)  # noqa: S101
 
-def test_csv_connector():
-    # Create test data and setup connector
-    test_data = dict({"col1": [1, 2, 3, 4, 5], "col2": ["a", "b", "c", "d", "e"]})
+
+def test_csv_connector() -> None:
+    """Test operations for the CSVConnector.
+
+    Tests init, check, read, and write.
+    """
+    test_data: dict[str, Any] = {
+        "col1": [1, 2, 3, 4, 5],
+        "col2": ["a", "b", "c", "d", "e"],
+    }
     data = PypeData(test_data)
-    test_file = Path('tests/test_csv_connector.csv')
+
+    test_file = Path("tests/test_csv_connector.csv")
     csv_connector = CSVConnector(test_file)
+
     # Test file doesn't exist
-    assert not test_file.exists()
+    assert not csv_connector.check()  # noqa: S101
+    assert not test_file.exists()  # noqa: S101
+
     # Test file exists after write
     csv_connector.write(data)
-    assert test_file.exists() and test_file.is_file()
+    assert csv_connector.check()  # noqa: S101
+
     # Test extracted data is the same as loaded
-    assert csv_connector.read().df.equals(data.df)
+    assert csv_connector.read().df.equals(data.df)  # noqa: S101
+
     # Clean-up
     test_file.unlink()
 
-    
-    
+
+def test_sql_connector() -> None:
+    """Test operations for the SQLConnector.
+
+    Tests init, check, read, write, and execute.
+    """
+    database_name = "testdb"
+    table, schema = ("dbo", "test_tbl")
+    database_table = f"{table}.{schema}"
+
+    # Create test data and setup connector
+    test_data: dict[str, Any] = {
+        "col1": [1, 2, 3, 4, 5],
+        "col2": ["a", "b", "c", "d", "e"],
+    }
+    data = PypeData(test_data)
+
+    connection_string = "mssql+pyodbc://@testdb?Trusted_Connection=yes"
+    sql_connector = SQLConnector(
+        con_str=connection_string,
+        db=database_name,
+        table=database_table,
+    )
+
+    # Cleanup testing environment
+    if inspect(sql_connector.engine).has_table(table):
+        sql_connector.execute(f"DROP TABLE {database_table}")
+
+    # Test data does not exist before write
+    assert not sql_connector.check()  # noqa: S101
+
+    # Test data exists after write
+    sql_connector.write(data)
+    assert sql_connector.check()  # noqa: S101
+
+    # Test extracted data is the same as loaded
+    assert sql_connector.read().df.equals(data.df)  # noqa: S101
+
+    # Cleanup testing environment
+    if inspect(sql_connector.engine).has_table(table):
+        sql_connector.execute(f"DROP TABLE {database_table}")
