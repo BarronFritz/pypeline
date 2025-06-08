f481b3c6bc97794a199858a6c2221d0595ea336f
Barron Fritz <barron.vonn.fritz@gmail.com>
2025-06-08 14:53:13 -0500

    comment and cleanup

--- src/pypeline/pipe.py
+++ src/pypeline/pipe.py
@@ -1,12 +1,28 @@
-from .connector import PypeConnector
+"""PypePipe class definition."""
+
+from pypeline.connector import PypeConnector
 
 
 class Pipe:
-    def __init__(self, extract: PypeConnector, loads: list[PypeConnector]):
+    """Simple extract and load Handler."""
+
+    def __init__(self, extract: PypeConnector, loads: list[PypeConnector]) -> None:
+        """Create a new Pipe.
+
+        Args:
+            extract (PypeConnector): Connector with check and read options.
+            loads (list[PypeConnector]): List of Connector's with extract option.
+
+        """
         self.extract = extract
         self.loads = loads
 
     def run(self) -> None:
+        """Perform internal operations in sequence.
+
+        Extract PypeData from 'extract' PypeConnector into a local reference.
+        For each loads PypeConnector, load extracted data to target destination.
+        """
         data = self.extract.read()
         for load in self.loads:
             load.write(data)
--- tests/test_pype.py
+++ tests/test_pype.py
@@ -1,29 +1,52 @@
-from pypeline.pipe import Pipe
-from pypeline.connector import DataConnector, CSVConnector
+"""Test Pipe class operations."""
+
 from pathlib import Path
+from typing import Any
+
+from sqlalchemy import inspect
+
+from pypeline.connectors.csv_connector import CSVConnector
+from pypeline.connectors.data_connector import DataConnector
+from pypeline.connectors.sql_connector import SQLConnector
+from pypeline.pipe import Pipe
 
 
-def test_pipe():
-    # Create test data
-    data = dict({"col1": [1, 2, 3, 4, 5], "col2": ["a", "b", "c", "d", "e"]})
-    # Extract "PypeData" from data dict
+def test_pipe() -> None:
+    """Test simple pipe operations."""
+    # Setup
+    data: dict[str, Any] = {"col1": [1, 2, 3, 4, 5], "col2": ["a", "b", "c", "d", "e"]}
     dict_extractor = DataConnector(data)
-    # Load to CSV file
+
     csv_path = Path("test.csv")
-    loader = CSVConnector(csv_path)
+    csv_loader = CSVConnector(csv_path)
+
+    connection_string = "mssql+pyodbc://@testdb?Trusted_Connection=yes"
+    database_name = "testdb"
+    schema_table = "dbo.test_tbl"
+    sql_loader = SQLConnector(
+        con_str=connection_string,
+        db=database_name,
+        table=schema_table,
+    )
+
     # Put operations into a pipe
-    pipe1 = Pipe(dict_extractor, [loader])
+    pipe1 = Pipe(dict_extractor, [csv_loader, sql_loader])
     # Assert that pipe doesn't throw an error
-    assert pipe1.run() is None
+    assert pipe1.run() is None  # noqa: S101
     # Assert csv file exists
-    assert csv_path.exists() and csv_path.is_file()
+    assert csv_path.exists()  # noqa: S101
+    assert csv_path.is_file()  # noqa: S101
     # Extract "PypeData" from csv file
     csv_extractor = CSVConnector(csv_path)
     # Assert csv_extractor confirms data is available
-    assert csv_extractor.check()
+    assert csv_extractor.check()  # noqa: S101
     # Assert that data from csv matches data written to csv
     data_1 = dict_extractor.read()
     data_2 = csv_extractor.read()
-    assert data_1.df.shape == data_2.df.shape  # type: ignore
+    assert data_1.df.shape == data_2.df.shape  # noqa: S101
     # Cleanup Test
     csv_path.unlink()
+    # Cleanup testing environment
+    _, table = schema_table.split(".")
+    if inspect(sql_loader.engine).has_table(table):
+        sql_loader.execute(f"DROP TABLE {schema_table}")
