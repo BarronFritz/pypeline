a0a9b897727184926f8375ce1dde3b4a26e4bd76
Barron Fritz <barron.vonn.fritz@gmail.com>
2025-06-12 20:51:47 -0500

    minor refactor

--- tests/test_connectors.py
+++ tests/test_connectors.py
@@ -26,16 +26,16 @@ def test_data_connector() -> None:
     extractor = DataConnector(data)
 
     # Test data exists
-    assert extractor.check()  # noqa: S101
+    assert extractor.check()
     test_data_2 = test_data
     test_data_2["col1"] = [6, 7, 8, 9, 10]
 
     # Test extractor data is the same as test_data
-    assert extractor.read().collect().equals(data.collect())  # noqa: S101
+    assert extractor.read().collect().equals(data.collect())
 
     # Not equal after write
     extractor.write(PypeData(test_data_2))
-    assert not extractor.read().collect().equals(data.collect())  # noqa: S101
+    assert not extractor.read().collect().equals(data.collect())
 
 
 def test_parquet_connector() -> None:
@@ -53,15 +53,15 @@ def test_parquet_connector() -> None:
     parquet_connector = ParquetConnector(test_file)
 
     # Test file doesn't exist
-    assert not parquet_connector.check()  # noqa: S101
-    assert not test_file.exists()  # noqa: S101
+    assert not parquet_connector.check()
+    assert not test_file.exists()
 
     # Test file exists after write
     parquet_connector.write(data)
-    assert parquet_connector.check()  # noqa: S101
+    assert parquet_connector.check()
 
     # Test extracted data is the same as loaded
-    assert parquet_connector.read().dataframe.equals(data.dataframe)  # noqa: S101
+    assert parquet_connector.read().dataframe.equals(data.dataframe)
 
     # Clean-up
     test_file.unlink()
@@ -82,15 +82,15 @@ def test_csv_connector() -> None:
     csv_connector = CSVConnector(test_file)
 
     # Test file doesn't exist
-    assert not csv_connector.check()  # noqa: S101
-    assert not test_file.exists()  # noqa: S101
+    assert not csv_connector.check()
+    assert not test_file.exists()
 
     # Test file exists after write
     csv_connector.write(data)
-    assert csv_connector.check()  # noqa: S101
+    assert csv_connector.check()
 
     # Test extracted data is the same as loaded
-    assert csv_connector.read().dataframe.equals(data.dataframe)  # noqa: S101
+    assert csv_connector.read().dataframe.equals(data.dataframe)
 
     # Clean-up
     test_file.unlink()
@@ -124,14 +124,14 @@ def test_sql_connector() -> None:
         sql_connector.execute(f"DROP TABLE {database_table}")
 
     # Test data does not exist before write
-    assert not sql_connector.check()  # noqa: S101
+    assert not sql_connector.check()
 
     # Test data exists after write
     sql_connector.write(data)
-    assert sql_connector.check()  # noqa: S101
+    assert sql_connector.check()
 
     # Test extracted data is the same as loaded
-    assert sql_connector.read().collect().equals(data.collect())  # noqa: S101
+    assert sql_connector.read().collect().equals(data.collect())
 
     # Cleanup testing environment
     if inspect(sql_connector.engine).has_table(table):
